3a4cf80ebcff8f2e5e2ea5bcb2bc00b26ae1c7c2
Kurnia Kharisma Agung Samiadjie <100695853+Kurniakun17@users.noreply.github.com>
2024-12-10 23:46:57 +0700

    Initial Commit

---
+++ GraffitiCraftTests/GraffitiCraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  GraffitiCraftTests.swift
+//  GraffitiCraftTests
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 10/12/24.
+//
+
+import Testing
+@testable import GraffitiCraft
+
+struct GraffitiCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
